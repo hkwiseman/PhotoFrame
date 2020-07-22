26109b7002ee5433545cefe13d8071bf993187c1
Kyle Wiseman <kwiseman@highpoint.edu>
2020-07-22 16:04:39 -0400

    Foundation for app

---
+++ PhotoFrame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFrame
+//
+//  Created by Kyle Wiseman on 7/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
